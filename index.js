c67f9ee0b8702211cf579f7facbf6b2b0cb2fd3b
Krystle Capuano <krystlecapuano@gmail.com>
2024-04-25 20:17:43 -0400

    "Font, style, and center items"

---
+++ index.js
@@ -0,0 +1,9 @@
+// Create your game here!
+
+// Alert
+// alert("\n Let's play a game!!!");
+
+// // Prompt
+// prompt(
+//   "A square is going to appear before you. \n Can you guess what color it will be?"
+// );
